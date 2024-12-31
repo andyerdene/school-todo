7f9170f2b3f1bbc7418cf3f77d483483e3a369e4
andyerdene <andyerdene@gmail.com>
2024-12-31 09:45:23 +0800

    active filter state

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import styles from "./page.module.css";
 export default function Home() {
   const [todos, setTodos] = useState(["a", "b"]);
   const [newTodo, setNewTodo] = useState("");
+  const [activeFilter, setActiveFilter] = useState("all");
 
   const addTodoHandler = () => {
     setTodos([...todos, newTodo]);
@@ -12,6 +13,7 @@ export default function Home() {
   const deleteHandler = () => {
     alert("are you sure to delete ?");
   };
+
   return (
     <div>
       <div className={styles[`todo-container`]}>
@@ -24,11 +26,25 @@ export default function Home() {
           />
           <button onClick={addTodoHandler}>Add</button>
         </div>
-        <div className={styles.flex}>
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
-          <button onClick={deleteHandler}>Delete</button>
+        <div className={`${styles.flex} ${styles.filterButtons}`}>
+          <button
+            className={activeFilter == "all" && styles.activeStyle}
+            onClick={() => setActiveFilter("all")}
+          >
+            All
+          </button>
+          <button
+            className={activeFilter == "active" && styles.activeStyle}
+            onClick={() => setActiveFilter("active")}
+          >
+            Active
+          </button>
+          <button
+            className={activeFilter == "completed" && styles.activeStyle}
+            onClick={() => setActiveFilter("completed")}
+          >
+            Completed
+          </button>
         </div>
         <div>
           {todos.map((todo, index) => {
